97f50a33780d2670b772507d32b4edea423b5469
mtalaga20 <michael.talaga@hotmail.com>
2021-10-27 23:31:46 -0500

    Added block class and snake movement

--- snakegame.py
+++ snakegame.py
@@ -20,7 +20,7 @@ height = 400
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Snake Game - Group 3")
 clock = pygame.time.Clock() 
-pace = 10 #Speed of the game
+pace = 30 #Speed of the game
 #define snake attributes
 #snakeBlocks = pygame.sprite.Group()
 
@@ -34,57 +34,107 @@ class SnakeGame:
      return 0
 
 
-class Snake(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-	self.x = x
-	self.y = y
-	self.speed = 10
-	self.width = 5
-	self.height = 5
-	#self.blocks = pygame.sprite.Group()
-	#self.blocks.add()
+class Block(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.id = 1 #block number
+		self.x = x
+		self.y = y
+		self.speed = 5
+		self.width = 5
+		self.height = 5
 	
 
-    def update(self):
-	#Add wasd
-	pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
-		self.y -= self.speed
-	if pressed[pygame.K_DOWN]:
-		self.y += self.speed  
-	if pressed[pygame.K_LEFT]:
-		self.x -= self.speed
-	if pressed[pygame.K_RIGHT]:
-		self.x += self.speed  
-	return 0
 
-    def render(self, screen):
-	#for each in blocks
-	#snakeBlocks.draw(screen)
-	pygame.draw.rect(screen, white, (self.x, self.y, self.width, self.height))
+class Snake(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.head = Block(200, 200)
+		self.testBlock = Block(200, 205)
+		self.testBlock2 = Block(200, 210)
+		self.blocks = pygame.sprite.Group()
+		self.blocks.add(self.head)
+		self.blocks.add(self.testBlock)
+		self.blocks.add(self.testBlock2)
+		self.direction = "NULL" #setting direction snke will move in
+	
+
+	def update(self):
+		pressed = pygame.key.get_pressed()
+		change = True
+		y_change = 0
+		x_change = 0
+		if pressed[pygame.K_UP]:
+			self.direction = "UP"
+		elif pressed[pygame.K_DOWN]:
+			self.direction = "DOWN"
+		if pressed[pygame.K_LEFT]:
+			self.direction = "LEFT"
+		elif pressed[pygame.K_RIGHT]:
+			self.direction = "RIGHT"
+		
+		if self.direction == "UP":
+			y_change -= self.head.speed
+		elif self.direction == "DOWN":
+			y_change += self.head.speed  
+		elif self.direction == "LEFT":
+			x_change -= self.head.speed
+		elif self.direction == "RIGHT":
+			x_change += self.head.speed
+		else:
+			change = False
+		#Function to update each block's location and adhere to snake movement rules
+		if change and len(self.blocks) > 1:
+			iter = 0
+			for block in self.blocks:
+				if iter == 0:
+					prevx = block.x
+					prevy = block.y
+				elif iter != 0:
+					currentx = block.x
+					currenty = block.y
+					block.x = prevx
+					block.y = prevy
+					prevx = currentx
+					prevy = currenty
+				
+				iter+=1
+			#Make change to head
+			self.head.x += x_change
+			self.head.y += y_change
+		
+
+				
+
+		#return 0
+	
+	def render(self, screen):
+		#for each in blocks
+		#snakeBlocks.draw(screen)
+		#pygame.draw.rect(screen, white, (self.x, self.y, self.width, self.height))
+		for block in self.blocks:
+			pygame.draw.rect(screen, white, (block.x, block.y, block.width, block.height))
 
 
 def game():
-    exit = False
-    snake = Snake(200, 200) #Initializes snake with a starting position
+	exit = False
+	snake = Snake(200, 200) #Initializes snake with a starting position
 
-    while not exit:
-	
-	for event in pygame.event.get():
-		if event.type==pygame.QUIT:
-			exit = True
+	while not exit:
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				exit = True
 		
 	
-        screen.fill(green)
+		screen.fill(green)
 	
 
 
 
-	snake.render(screen)
-	snake.update()
-        pygame.display.update()
-        clock.tick(pace)
+		snake.render(screen)
+		snake.update()
+		pygame.display.update()
+		clock.tick(pace)
     
 def main():
     game()
