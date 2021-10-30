c57697206b3585c709a96708b9a86a093719bd5d
Adam Abernathy <amabernathy@ku.edu>
2021-10-30 10:18:50 -0500

    added score

--- snakegame.py
+++ snakegame.py
@@ -7,6 +7,7 @@ import time
 #external files
 from Block import Block
 from snake import Snake
+from Food import Food
 
 pygame.init()
 
@@ -19,12 +20,14 @@ by: Morgan Marino, Michael Talaga
 green = (0, 255, 0)
 white = (255, 255, 255)
 #define display settings
-width = 500
-height = 400
-screen = pygame.display.set_mode((width, height))
+display_width = 500
+display_height = 400
+screen = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Snake Game - Group 3")
 clock = pygame.time.Clock()
 pace = 30 #Speed of the game
+# initial score
+score = 0
 #define snake attributes
 #snakeBlocks = pygame.sprite.Group()
 
@@ -50,6 +53,20 @@ class SnakeGame:
     def add_tail():
          return 0
 
+
+
+# displaying Score function
+def show_score(choice, color, font, size):
+    # creating font object score_font
+    score_font = pygame.font.SysFont(font, size)
+    # create the display surface object
+    score_surface = score_font.render('Score : ' + str(score), True, color)
+    # create a rectangular object for the
+    # text surface object
+    score_rect = score_surface.get_rect()
+    # displaying text
+    screen.blit(score_surface, score_rect)
+
 def game():
     '''
     COMMENTS \n
@@ -60,6 +77,8 @@ def game():
     '''
     exit = False
     snake = Snake(200, 200) #Initializes snake with a starting position
+    #random food placement
+    food = Food(random.randrange(1, display_width//10) * 10, random.randrange(1, display_height//10) * 10)
 
     while not exit:
         for event in pygame.event.get():
@@ -71,6 +90,8 @@ def game():
         screen.fill(green)
         snake.render(screen)
         snake.update()
+        # display score
+        show_score(score, white, 'times new roman', 20)
         pygame.display.update()
         clock.tick(pace)
 
@@ -78,7 +99,7 @@ def main():
     '''
     Main file to run the game and exit the system when it is finished. \n
     Autor: Michael Talaga
-    
+
     '''
     game()
     pygame.quit()
