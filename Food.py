aafe2e029877d02c47fde929665d9c4972daf3e6
mtalaga20 <michael.talaga@hotmail.com>
2021-10-30 14:28:48 -0500

    Food rendering

--- Food.py
+++ Food.py
@@ -37,3 +37,17 @@ class Food(pygame.sprite.Sprite):
         self.y = y
         self.width = 5
         self.height = 5
+    
+    def changePosition(self, screenDimensions):
+        '''
+        Change the position of food upon collision with snake head, ensure it is at snake head
+        :param screenDimensions: dimensions for the screen
+        :type screenDimensions" tuple as (x, y) for (width, height)
+        '''
+        self.x = random.randrange(1, screenDimensions[0]-10)
+        self.y = random.randrange(1, screenDimensions[1]-10)
+
+    #def update(self, screenDimensions):
+
+    def render(self, screen, color):
+        pygame.draw.rect(screen, color, (self.x, self.y, self.width, self.height))
