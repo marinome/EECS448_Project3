3b4a03e5a509c7b7e26dc6345124b285da1d88d3
Adam Abernathy <amabernathy@ku.edu>
2021-11-08 22:58:33 -0600

    started test class

---
+++ test_snake.py
@@ -0,0 +1,20 @@
+import unittest
+
+'''
+unit test class
+date: nov 8 2021
+'''
+
+class TestSnake(unittest.TestCase):
+'''
+Test suite for Snake class
+
+Example::
+
+    python3 test_snake.py
+
+'''
+    def test_one(self):
+        some_input = 5
+        some_output = 6
+        print(some_input + some_output)
